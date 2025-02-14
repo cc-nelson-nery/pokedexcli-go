d9b0bd4374aae96e52c8a5b1802567467494e16d
cc-nelson-nery <nelson.nery@finoservices.com>
2025-02-14 13:56:09 -0500

    CHA2: Cache -> L4: Catch

--- command_catch.go
+++ command_catch.go
@@ -5,11 +5,22 @@ import (
 	"math/rand"
 )
 
+var pokedex = make(map[string]struct {
+	Id             int
+	Name           string
+	BaseExperience int
+})
+
 func commandCatch(cfg *Config) error {
 	fmt.Printf("Throwing a Pokeball at %v...\n", *cfg.arg)
 	pokemon, err := cfg.pokeapiClient.GetPokemon(cfg.arg)
 	if err != nil {
 		return err
+	}
+
+	if _, ok := pokedex[pokemon.Name]; ok {
+		fmt.Printf("%s found in pokedex\n", pokemon.Name)
+		return nil
 	}
 
 	chance := rand.Intn(pokemon.BaseExperience) + 20
@@ -18,6 +29,16 @@ func commandCatch(cfg *Config) error {
 		fmt.Printf("%s escaped!\n", pokemon.Name)
 	} else {
 		fmt.Printf("%s was caught!\n", pokemon.Name)
+
+		pokedex[pokemon.Name] = struct {
+			Id             int
+			Name           string
+			BaseExperience int
+		}{
+			Id:             pokemon.Id,
+			Name:           pokemon.Name,
+			BaseExperience: pokemon.BaseExperience,
+		}
 	}
 
 	return nil
